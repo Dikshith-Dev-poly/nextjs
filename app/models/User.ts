f0bdaf856c1d0634ce5b351f35e6f0ec12855400
Dikshith Devadiga <devadigadikshith1@gmail.com>
2026-04-28 19:11:14 +0530

    Added user model

---
+++ app/models/User.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+
+interface IUser extends Document {
+    name: string,
+    email: string,
+    deleted: boolean,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, index: true, lowercase: true, match: [/^\S+@\S+\.\S+$/, "Invalid email"] },
+    deleted: { type: Boolean, default: false }
+}, { timestamps: true });
+
+
+
+const User: Model<IUser> = (mongoose.models.User as Model<IUser>) || mongoose.model<IUser>("User", userSchema);
+export default User;
